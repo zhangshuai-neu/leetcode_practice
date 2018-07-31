32002d113c098fb7c79a5471b73be91005cbe518
zhangshuai <zhangshuaiisme@gmail.com>
2018-07-31 18:30:47 +0800

    easy question 771

---
+++ 1week/easy/771_accept.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        
+        count = 0;
+        j_len = len(J);
+        s_len = len(S);
+
+        #建一个词典
+        stone_type_dict = {};
+
+        for i in range(j_len):
+            stone_type_dict[J[i]]=0;
+
+        for i in range(s_len):
+            if S[i] in stone_type_dict:
+                stone_type_dict[S[i]] = stone_type_dict[S[i]] + 1
+
+        for i in range(j_len):
+            count = stone_type_dict[J[i]] + count;
+        
+        return count;
+        
