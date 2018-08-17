08a2637020c50cf651e13482016f979e2da74c63
zhangshuai <zhangshuaiisme@gmail.com>
2018-08-17 19:39:24 +0800

    51 accept

--- 2_week/hard/51_accept.py
+++ 2_week/hard/51_accept.py
@@ -22,53 +22,47 @@ class Solution:
         #递归获取结果
         def get_solution(chess_list, new_row, n):
             solution_list = []
-            if new_row == n-1:
+            if new_row >= n-1:
                 #最后一行
                 for col in range(n):
                     if is_valid(chess_list, new_row, col, n):
                         chess_list.append([new_row, col])
                         #一旦到达最后一行，就只可能有一个正确结果
-                        print(chess_list)
-                        return chess_list
-                #没有正确结果
-                return None
+                        new_chess_list = chess_list.copy()
+                        solution_list.append(new_chess_list)
+                        chess_list.pop()
+                return solution_list
             else:
             #其他列
                 for col in range(n):
                     if is_valid(chess_list, new_row, col, n):
-                        
                         chess_list.append([new_row,col])
                         temp_solution_list=get_solution(chess_list, new_row+1, n)
-                        if temp_solution_list != None:
-                            for i in range(len(temp_solution_list)):
-                                solution_list.append(temp_solution_list[i])
+                        for i in range(len(temp_solution_list)):
+                            solution_list.append(temp_solution_list[i])
                         chess_list.pop()
-
             return solution_list
+            
         
         #将chess_list变成指定形式
         def chess_list_to_str(chess_list):
             solution_str_list= []
             for i in range(len(chess_list)):
                 temp_str_list = [ "." for i in range(n)]
-                j=chess_list[1]
-                temp_str_list[j]='Q'
+                temp_str_list[chess_list[i][1]]='Q'
                 temp_str = "".join(temp_str_list)
                 solution_str_list.append(temp_str)
             return solution_str_list
 
         #处理
         chess_list = []
+        solution_list_str = []
         solution_list = get_solution(chess_list,0,n)
-        solution = []
-        print(solution_list)
         for i in range(len(solution_list)):
-            solution.append(chess_list_to_str(solution_list[i]))
+            solution_list_str.append(chess_list_to_str(solution_list[i]))
         
-        return solution
+        return solution_list_str
 
-s = Solution()
-s.solveNQueens(4)
 
         
         
