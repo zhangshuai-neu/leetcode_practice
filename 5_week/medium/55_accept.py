3c74d9928b928fb215697f9366f047910b1e0318
ZhangShuai <zhangshuaiisme@gmail.com>
2018-09-10 19:09:08 +0800

    medium 55 accept

--- 5_week/medium/55_accept.py
+++ 5_week/medium/55_accept.py
@@ -4,4 +4,31 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
+        # 使用贪心法来解决
+        # 每次走尽可能的远（局部最优）
+        # 最后走的最远（全局最优）
+        # notice：只要能够到达的位置(reach)比last_index大即可
+        last_index = len(nums)-1
+        reach = nums[0]
+        i = 0
+        while i<=reach and reach<last_index:
+            reach = max(reach, i+nums[i])
+            i=i+1
+
+        print(reach>=last_index)    #debeg code
         
+        return reach>=last_index
+
+#-------------------------------------
+# 测试代码
+#-------------------------------------
+s = Solution()
+
+s.canJump([1,2,3])
+# return Ture
+
+s.canJump([3,2,1,0,4])
+# return False
+
+s.canJump([2,3,1,1,4])
+# return True
