d306cf2f9811d30196ea4aef6ff80ca312f68007
zhangshuai <zhangshuaiisme@gmail.com>
2018-09-21 21:08:08 +0800

    155 accept

--- 5_week/easy/155_accept.py
+++ 5_week/easy/155_accept.py
@@ -4,6 +4,7 @@ class MinStack:
         """
         initialize your data structure here.
         """
+        self.list = []
         
 
     def push(self, x):
@@ -11,30 +12,34 @@ class MinStack:
         :type x: int
         :rtype: void
         """
+        self.list.append(x)
         
 
     def pop(self):
         """
         :rtype: void
         """
+        self.list.pop()
         
 
     def top(self):
         """
         :rtype: int
         """
+        return self.list[len(self.list)-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
+        return min(self.list)
         
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+#-------------------------------------
+# 测试代码
+#-------------------------------------
+obj = MinStack()
+obj.push(x)
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
