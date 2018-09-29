924a8ca911836ccc5eaddb91fc804001063958f4
zhangshuai <zhangshuaiisme@gmail.com>
2018-09-29 10:08:37 +0800

    146 accept

--- 6_week/hard/146_accept.py
+++ 6_week/hard/146_accept.py
@@ -21,8 +21,9 @@ class LRUCache:
         :rtype: int
         """
         if key in self.lru_map:
-            self.put(key,self.lru_map[key].data)
-            return self.lru_map[key].data
+            node = self.lru_map[key]
+            self.put(node.key,node.data)
+            return node.data
         else:
             return -1
 
@@ -44,19 +45,19 @@ class LRUCache:
 
         if key in self.lru_map:
             node = self.lru_map[key]
-            #提出
-            prev_node = node.prev
-            next_node = node.next
-            prev_node.next = next_node
-            next_node.prev = prev_node
-            #插入原来的head之前
-            head_prev_node = self.head.prev
-            head_prev_node.next = node
-            node.prev = head_prev_node
-            node.next = self.head
-            self.head.prev = node
-
-            self.head = node
+            if self.head.key != node.key:
+                #提出
+                prev_node = node.prev
+                next_node = node.next
+                prev_node.next = next_node
+                next_node.prev = prev_node
+                #插入原来的head之前
+                head_prev_node = self.head.prev
+                head_prev_node.next = node
+                node.prev = head_prev_node
+                node.next = self.head
+                self.head.prev = node
+                self.head = node
             node.data = value
         else:
             node = LRUCache.link_node(key,value)
@@ -68,6 +69,7 @@ class LRUCache:
             node.next = self.head
             self.head.prev = node
             self.head = node
+            
             self.len=self.len +1
             #过多
             if self.len>self.capacity:
@@ -79,18 +81,19 @@ class LRUCache:
                 self.len=self.len-1
 
     def print_all(self):
-        print(self.head.key)
-        head=self.head.next
-        while head!=self.head:
-            print(head.key)
+        head=self.head
+        print("list_key:",end="")
+        for i in range(self.len):
+            print(head.key," ",end="")
             head=head.next
+        print()
 
 #==========================================
 # 测试代码
 #==========================================
 
 
-
+""" test1
 cache = LRUCache(2);
 
 cache.put(1, 11)
@@ -116,3 +119,25 @@ print(cache.get(3))
 
 print(cache.get(4))             
 # returns 4
+"""
+
+func_list = ["put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
+
+parameter_list = [[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+
+cache = LRUCache(10)
+for i in range(len(func_list)):
+    if func_list[i]=="put":
+        print("put:",parameter_list[i][0],parameter_list[i][1])
+        cache.put(parameter_list[i][0],parameter_list[i][1])
+    else:
+        print("get:",parameter_list[i][0])
+        cache.get(parameter_list[i][0])
+    
+    cache.print_all()
+    print()
+    
+    
+    
+    
+    
