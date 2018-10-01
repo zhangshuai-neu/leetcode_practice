00a389543bff94d62a7ef742b9830b707217b1a4
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-01 18:43:25 +0800

    561 accept

--- 7_week/easy/561_accept.py
+++ 7_week/easy/561_accept.py
@@ -4,4 +4,18 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        nums_len = len(nums)
+        nums.sort()
         
+        min_pari_sum = 0
+        for i in range(nums_len//2):
+            min_pari_sum = min_pari_sum + nums[i*2]
+        
+        return min_pari_sum
+        
+#==========================
+# 测试程序
+#==========================
+s = Solution()
+
+s.arrayPairSum([1,4,3,2])
