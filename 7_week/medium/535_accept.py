27e56d052371b87b80027eff473579b7a2ee5e60
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-05 22:03:43 +0800

    535 some

--- 7_week/medium/535_accept.py
+++ 7_week/medium/535_accept.py
@@ -1,21 +1,48 @@
 class Codec:
+    def __init__(self):
+        self.l2s_map = {}
+        self.s2l_map = {}
+        self.hash_dict = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
 
-    def encode(self, longUrl):
-        """Encodes a URL to a shortened URL.
         
+
+    def encode(self, long_url):
+        """Encodes a URL to a shortened URL.
         :type longUrl: str
         :rtype: str
         """
-        
+        def hash_code():
+            hash_str = ""
+            for i in range(6):
+                index = random.randint(0,61)
+                hash_str = hash_str+self.hash_dict[index]    
+            print(hash_str)
+            return hash_str
+            
+        short_url = hash_code()
+        while short_url in self.s2l_map:
+            short_url = hash_code()
 
-    def decode(self, shortUrl):
-        """Decodes a shortened URL to its original URL.
+        self.l2s_map.update({long_url:short_url})
+        self.s2l_map.update({short_url:long_url})
         
+        return "https://tinyurl.com/"+short_url
+
+    def decode(self, short_url):
+        """Decodes a shortened URL to its original URL.
         :type shortUrl: str
         :rtype: str
         """
-        
+        return self.s2l_map.get(short_url)
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(url))
+
+#============================
+# 测试代码
+#============================
+import random
+c =  Codec()
+
+s = c.encode("https://leetcode.com/problems/design-tinyurl")
+
+s1 = c.decode(s)
