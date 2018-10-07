20d37bdbd00f2a457013a3ecda5263e52651664b
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-07 14:38:54 +0800

    654 accept

--- 7_week/medium/654_accept.py
+++ 7_week/medium/654_accept.py
@@ -10,5 +10,23 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: TreeNode
-        """
+        """        
+        nums_len = len(nums)
+        if nums_len==0:
+            return None
+            
+        max_index = 0
+        max_num = nums[max_index]
+        for i in range(nums_len):
+            if nums[i] > max_num:
+                max_num = nums[i]
+                max_index  = i
+        
+        left = self.constructMaximumBinaryTree(nums[:max_index])
+        right = self.constructMaximumBinaryTree(nums[max_index+1:])
+        root = TreeNode(max_num)
+        root.left = left
+        root.right = right
+        
+        return root
         
