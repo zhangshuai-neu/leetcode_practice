914409f8a70073cc8815f7cbd6a477059794df01
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-15 19:25:38 +0800

    403 accept

--- 8_week/medium/238_accept.py
+++ 8_week/medium/238_accept.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        #没有出发，在O(n)
+        nums_len = len(nums)
+        
+        
+        
         
