53849ea59119c6a56b758a7aea2e7ee8aee8a50b
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-15 20:37:58 +0800

    238 accept

--- 8_week/medium/238_accept.py
+++ 8_week/medium/238_accept.py
@@ -4,9 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        #没有出发，在O(n)
         nums_len = len(nums)
-        
-        
-        
-        
+        #创建left
+        left  = []
+        product = 1
+        for i in range(nums_len):
+            product = product*nums[i]
+            left.append(product)
+        #创建right
+        right = []
+        product = 1
+        j = nums_len-1
+        while j>-1:
+            product = nums[j]*product
+            right.insert(0,product)
+            j=j-1
+        #计算
+        result = []
+        for i in range(nums_len):
+            result.append(1)
+            if i-1>=0:
+                result[i] = result[i]*left[i-1]
+            if i+1<nums_len:
+                result[i] = result[i]*right[i+1]
+        return result
+
+#=====================
+# 测试代码
+#=====================
+s = Solution()
+s.productExceptSelf([1,2,3,4])
+s.productExceptSelf([0,0])
