556e38574476f0b22cba5c47606e622f6d1c185a
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-16 19:45:33 +0800

    add 9 week questions

---
+++ 9_week/medium/894_accept.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def allPossibleFBT(self, N):
+        """
+        :type N: int
+        :rtype: List[TreeNode]
+        """
+        
