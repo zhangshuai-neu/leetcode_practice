1807f85055f1e2d4c1525b671fe896eba4eaf6d8
zhangshuai <zhangshuaiisme@gmail.com>
2018-10-19 19:48:14 +0800

    701 accept

--- 9_week/medium/701_accept.py
+++ 9_week/medium/701_accept.py
@@ -12,4 +12,30 @@ class Solution(object):
         :type val: int
         :rtype: TreeNode
         """
+        origin_root = root
+        new_node=TreeNode(val)
+        
+        if origin_root==None:
+            origin_root = new_node
+            return origin_root
+        
+        temp_root = origin_root
+        while True:
+            if val <temp_root.val:
+                if temp_root.left==None:
+                    temp_root.left = new_node
+                    break
+                else:
+                    temp_root=temp_root.left
+            else:
+                if temp_root.right==None:
+                    temp_root.right = new_node
+                    break
+                else:
+                    temp_root=temp_root.right
+        
+        return origin_root
+            
+        
+            
         
