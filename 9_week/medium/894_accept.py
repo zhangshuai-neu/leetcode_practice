42d38cf42a6004d3a1702fe34c3cabbc310def2f
ZhangShuai <zhangshuaiisme@gmail.com>
2018-10-23 13:55:16 +0800

    some

--- 9_week/medium/894_accept.py
+++ 9_week/medium/894_accept.py
@@ -11,4 +11,24 @@ class Solution(object):
         :type N: int
         :rtype: List[TreeNode]
         """
+        global_result_list = []
+        if N%2==0:
+            #不可能
+            return global_result_list
+
+        if N==1:
+            t=TreeNode(0)
+            global_result_list.append(t)
+            return global_result_list
         
+        # N>=3
+        # 逐步生成full binary tree，每次添加两个节点
+        # n当前节点数，N最大节点数
+        # 每次生成一个root，都要重新记录
+        def add_node_in_fbt(root,)
+        new_root=TreeNode(0)
+
+        
+            
+
+
