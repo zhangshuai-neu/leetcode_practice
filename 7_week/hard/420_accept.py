fdf126545ec94da31d75af082044019d82ff216d
ZhangShuai <zhangshuaiisme@gmail.com>
2018-10-24 13:49:50 +0800

    420 some

--- 7_week/hard/420_accept.py
+++ 7_week/hard/420_accept.py
@@ -26,7 +26,14 @@ class Solution:
         for i in range(s_len):
             char_map[s[i]] = char_map[s[i]]+1
         char_usage = list(char_map.values())
-        if sum(char_usage[0:10])>0 and sum(char_usage[10:26])>0 and sum(char_usage[26:62])>0
+        c2_count = 0
+        if sum(char_usage[0:10])<=0:
+            c2_count = c2_count+1 
+        if sum(char_usage[10:26])<=0:
+            c2_count = c2_count+1
+        if sum(char_usage[26:62])<=0:
+            c2_count = c2_count+1
+        if c2_count<=0:
             flag2=True
         
         # 第3个条件
@@ -34,7 +41,40 @@ class Solution:
         while i<s_len-2:
             if s[i]==s[i+1]==s[i+2]:
                 flag3 = False
-        
+
+        # 确定修改次数
+        if flag1:
+            if flag2:
+                if flag3:
+                    # strong
+                    return 0
+                else:
+            else:
+                if flag3:
+                    # 2异常
+                    # 使用replace
+                    return c2_count
+                else:
+                    # 2,3异常
+
+        else：
+            if flag2:
+                if flag3:
+                    # 1异常
+                    if s_len>20:
+                        # 使用删除操作
+                        return s_len-20
+                    if s_len<6:
+                        # 使用插入操作
+                        return 6-s_len
+                else:
+                    # 1,3异常
+            else:
+                if flag3:
+                    # 1,2异常
+                    
+                else:
+                    # 1,2,3异常
         
         
         
