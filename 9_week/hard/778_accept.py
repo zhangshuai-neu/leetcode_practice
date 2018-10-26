b8a62fedf8aa3853233a84727f54b04f3bc7b9c6
ZhangShuai <zhangshuaiisme@gmail.com>
2018-10-26 08:45:22 +0800

    778 some

--- 9_week/hard/778_accept.py
+++ 9_week/hard/778_accept.py
@@ -4,58 +4,42 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        N = len(grid)
-        
-        #生成可达矩阵
-        reach_matrix = []
-        for i in range(N):
-            temp_list = [ 0 for i in range(N)]
-            reach_matrix.append(temp_list)
-        reach_matrix[0][0]=1
-        
-        #未达dict
-        unreach_dict = {}
-        for i in range(1,N*N):
-            unreach_dict.update({i:1})
-        
-        h0 = grid[0][0]
-        
-        #随着t增加，变化可达性
-        t=0
-        while reach_matrix[N-1][N-1]!=1:
-            change_flag = True
-            while change_flag:
-                change_flag = False
-                key_list = list(unreach_dict.keys())
-                for l in key_list:
-                    i=l//N
-                    j=l%N
-                    have_1_flag = False
-                    if not have_1_flag and i-1>=0 and reach_matrix[i-1][j]==1:
-                        have_1_flag=True
-                    if not have_1_flag and i+1<N and reach_matrix[i+1][j]==1:
-                        have_1_flag=True
-                    if not have_1_flag and j-1>=0 and reach_matrix[i][j-1]==1:
-                        have_1_flag=True
-                    if not have_1_flag and j+1<N and reach_matrix[i][j-1]==1:
-                        have_1_flag=True
-                    #周围有1
-                    if have_1_flag and max(t,h0)==max(t,grid[i][j]):
-                        #可达                  
-                        reach_matrix[i][j]=1
-                        unreach_dict.pop(l)
-                        change_flag = True
-            t=t+1
+        #深度遍历 判断能否到达 grid[N-1][N-1]
+        def deep_search(grid,t,si,sj,pre_map):
+            N=len(grid)
 
-        return t-1
+            
+            return result
+
+        # t一定在 0 和 max(grid)之间
+        N = len(grid)
+        max_t = 0
+        for i in range(N):
+            max_t=max( max_t, max(grid[i]) )
+
+        pre_map={}
+
+
+        big_t=max_t
+        small_t=0
+        mid_t=(big_t+small_t)//2
+        while small_t<big_t:
+            if deep_search(grid,mid_t,0,0,pre_map):
+                big_t=mid_t
+            else:
+                small_t=mid_t+1
+            mid_t=(big_t+small_t)//2
+
+        return mid_t
         
 #=========================================
 s=Solution()
+'''
 print(s.swimInWater([[0,2],[1,3]]))
 #return 3
 
 print(s.swimInWater([[3,2],[0,1]]))
 #return 3
-
-        
-             
+'''
+l=[[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+print(s.swimInWater(l))
