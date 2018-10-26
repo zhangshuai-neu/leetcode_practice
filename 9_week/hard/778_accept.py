0cac2c9158b90d9bee0705a8b1811fca2858511f
ZhangShuai <zhangshuaiisme@gmail.com>
2018-10-26 12:59:47 +0800

    778 accept

--- 9_week/hard/778_accept.py
+++ 9_week/hard/778_accept.py
@@ -5,31 +5,50 @@ class Solution(object):
         :rtype: int
         """
         #深度遍历 判断能否到达 grid[N-1][N-1]
-        def deep_search(grid,t,si,sj,pre_map):
+        def deep_search(grid,t,i,j,reach_matrix):
             N=len(grid)
-
-            
+            if reach_matrix[N-1][N-1]==1:
+                return True
+            result = False
+            next = [[1,0],[-1,0],[0,1],[0,-1]]
+            for step in range(4):
+                next_i = i+next[step][0] 
+                next_j = j+next[step][1] 
+                if 0<=next_i and next_i<N and 0<=next_j and next_j<N:
+                    h=grid[0][0]
+                    next_h=grid[next_i][next_j]
+                    if reach_matrix[next_i][next_j]==0 and t>=h and t>=next_h:
+                        #reach_matrix[next_i][next_j]==0 避免重复
+                        reach_matrix[next_i][next_j]=1
+                        result = result or deep_search(grid,t,next_i,next_j,reach_matrix)
+                if result:
+                    break
             return result
 
-        # t一定在 0 和 max(grid)之间
+        #生成可达矩阵，N^N
+        def init_reach_matrix(N):
+            reach_matrix=[]
+            for i in range(N):
+                temp_list = [0 for j in range(N)]
+                reach_matrix.append(temp_list)
+            reach_matrix[0][0]=1
+            return reach_matrix
+
+        # t一定在 0 和 N*N-1之间(题目给出)
         N = len(grid)
-        max_t = 0
-        for i in range(N):
-            max_t=max( max_t, max(grid[i]) )
-
-        pre_map={}
-
-
+        max_t = N*N-1
         big_t=max_t
         small_t=0
         mid_t=(big_t+small_t)//2
         while small_t<big_t:
-            if deep_search(grid,mid_t,0,0,pre_map):
+            reach_matrix = init_reach_matrix(N);
+
+            if deep_search(grid,mid_t,0,0,reach_matrix):
                 big_t=mid_t
             else:
                 small_t=mid_t+1
             mid_t=(big_t+small_t)//2
-
+        
         return mid_t
         
 #=========================================
