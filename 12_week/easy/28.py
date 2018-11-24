1d4fc75823decd918395b11aa09cb352795979e8
ShuaiZhang <zhangshuaiisme@gmail.com>
2018-11-24 16:30:07 +0800

    28 25 ok

--- 12_week/easy/28.py
+++ 12_week/easy/28.py
@@ -5,4 +5,18 @@ class Solution:
         :type needle: str
         :rtype: int
         """
+        #使用简单方法，KMP比较快
+        if needle == "":
+            return 0
         
+        len_haystack = len(haystack)
+        len_needle = len(needle)
+        if len_needle>len_haystack:
+            return -1
+
+        # +1 避免出现"a"和"a"这种情况
+        for i in range(len_haystack-len_needle+1):
+            if haystack[i]==needle[0]:
+                if haystack[i:i+len_needle] == needle:
+                    return i
+        return -1
--- 12_week/easy/35.py
+++ 12_week/easy/35.py
@@ -6,3 +6,26 @@ class Solution:
         :rtype: int
         """
         
+        left = 0
+        right = len(nums)-1
+        
+        # 特殊情况，过大或者过小
+        if target>nums[right]:
+            return right+1
+            
+        if target<nums[left]:
+            return 0
+            
+        # 二分查找
+        while left<right:
+            mid = left//2 + right//2
+            
+            if target == nums[mid]:
+                return mid
+            
+            if target > nums[mid]:
+                left = mid+1
+            else:
+                right = mid
+        return right
+        
