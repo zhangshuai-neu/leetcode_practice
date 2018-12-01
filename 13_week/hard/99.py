d19bdd3cfdf99ec5ea274e615ef0d07a00cc3d0f
ShuaiZhang <zhangshuaiisme@gmail.com>
2018-12-01 19:34:15 +0800

    add 13 week question

---
+++ 13_week/hard/99.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        
---
+++ 13_week/medium/576.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPaths(self, m, n, N, i, j):
+        """
+        :type m: int
+        :type n: int
+        :type N: int
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        
