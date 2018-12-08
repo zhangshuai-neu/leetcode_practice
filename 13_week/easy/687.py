09e80337447e96f70a9895d3eb6b953f9088c8a6
zhangshuai <zhangshuaiisme@gmail.com>
2018-12-08 18:59:59 +0800

    some

--- 13_week/easy/687.py
+++ 13_week/easy/687.py
@@ -12,3 +12,21 @@ class Solution:
         :rtype: int
         """
         
+        """
+            5(a)
+            /
+           5(b)
+         /    \
+        5(c)   5(d)
+        
+        经过某一节点的最长的path，可能有三种情况
+        a-b-c
+        a-b-d
+        c-b-d
+        需要计算三种情况的最大值，
+        用deep记录 a-b 的长度
+        用left记录 b-c 的长度
+        用right记录 b-d 的长度
+        """
+        
+        
