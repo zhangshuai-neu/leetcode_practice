3a816a5493882935b9efeb287934cc23499b020b
ShuaiZhang <zhangshuaiisme@gmail.com>
2018-12-11 11:09:22 +0800

    687 accept

--- 13_week/easy/687.py
+++ 13_week/easy/687.py
@@ -24,9 +24,46 @@ class Solution:
         a-b-d
         c-b-d
         需要计算三种情况的最大值，
-        用deep记录 a-b 的长度
-        用left记录 b-c 的长度
-        用right记录 b-d 的长度
+        记录 a-b 的长度
+        记录 b-c 的长度
+        记录 b-d 的长度
+        
         """
+        def path(root,val_dict):
+            if root.val not in val_dict:
+                val_dict.update({root.val:0})
+                
+            if root.left==None and root.right==None:
+                return 0
+            # 左侧
+            left_val = 0
+            if root.left!=None:
+                if root.left.val == root.val:
+                    left_val = path(root.left,val_dict)+1
+                else:
+                    path(root.left,val_dict)
+                    
+            # 右侧     
+            right_val = 0
+            if root.right!=None:
+                if root.right.val == root.val:
+                    right_val = path(root.right,val_dict)+1
+                else:
+                    path(root.right,val_dict)
+            
+            # 更新dict
+            # 处理了多种情况，c-b-d形式
+            # 1) right_val==0,left_val!=0
+            # 2) right_val!=0,left_val==0
+            # 3) right_val!=0,left_val!=0
+            if root.val in val_dict  and val_dict[root.val]< right_val+left_val:
+                val_dict[root.val] = right_val+left_val
+            
+            # 父亲节点需要这个数字 a-b-d 或者 a-b-c 形式
+            return max(left_val,right_val)
         
-        
+        val_dict = {}
+        if root!=None:
+            path(root,val_dict)
+            return max(val_dict.values())
+        return 0
