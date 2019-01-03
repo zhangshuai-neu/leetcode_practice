032bd9279b9b763be962f3c0cadf66de3c9aaaed
ShuaiZhang <zhangshuaiisme@gmail.com>
2019-01-03 20:45:44 +0800

    944 accept

--- 16_week/easy/944.py
+++ 16_week/easy/944.py
@@ -4,4 +4,22 @@ class Solution:
         :type A: List[str]
         :rtype: int
         """
+        if A==None:
+            return 0
+        
+        count = 0
+        A_len = len(A)
+        str_len = len(A[0])
+        for column in range(str_len):
+            for i in range(A_len):
+                if i+1<A_len and ord(A[i+1][column])<ord(A[i][column]):
+                    count = count+1
+                    break
+        return count
+
+s = Solution()
+print(s.minDeletionSize(["cbao", \
+                         "dafp", \
+                         "ghiq"]))
+        
         
