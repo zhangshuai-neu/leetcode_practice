05a0065a26cd6d529041abffec7a4bc6fd6d90cf
ShuaiZhang <zhangshuaiisme@gmail.com>
2019-01-17 10:27:48 +0800

    868 accept

--- 20_week/868.c
+++ 20_week/868.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 
 int binaryGap(int N) {
-    int max_distance = 0;
+    int max_d = 0;  //max_distance
     const int AND_NUM=1;
     int count = 0;
     while(N!=0){
+        count=0;
+        //printf("%d\n",N);
         if(N&AND_NUM==1){
-            sign=1;
-            
+            count++;
+            N=N>>1;
+            // 按位与的优先级比较低
+            while(N!=0 && (N&AND_NUM)==0){
+                count++;
+                N=N>>1;
+            }
+            if(N!=0 && N&AND_NUM==1){
+                max_d = max_d>count? max_d:count;
+                continue;
+            }
+            N=N>>1;
+        } else {
+            N=N>>1;
         }
-        if(N&AND_NUM==0){
-            count = count+1
-        }
-        N=N>>1;
     }
-    return max_distance;
+    return max_d;
 }
 
 int main(){
-    int N = 9 ;
-    int ret = binaryGap(N);
-    printf("ret:%d\n",ret);
+    printf("ret:%d\n",binaryGap(22));
+    printf("ret:%d\n",binaryGap(5));
+    printf("ret:%d\n",binaryGap(6));
 }
