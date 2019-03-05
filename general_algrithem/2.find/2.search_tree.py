73b7e642ff6bc9fb316b003e483584c4e5a55f34
Shuai Zhang <zhangshuaiisme@gmail.com>
2019-03-05 11:01:38 +0800

    some code of search_tree

---
+++ general_algrithem/2.find/2.search_tree.py
@@ -0,0 +1,95 @@
+#=====================
+# search tree 节点类
+#=====================
+class SearchTreeNode:
+    val = 0
+    key = 0 # 可以是任何类型, 不可修改
+    left = None
+    right = None
+    # 统计左右子树的节点数量
+    left_num = 0
+    right_num = 0
+    
+    def __init__(self, key ,val):
+        self.val = val
+        self.key = 0
+        self.left = None
+        self.right = None
+        self.left_num = 0
+        self.right_num = 0
+    
+    def get_key(self):
+        return self.key
+    
+    def get_val(self):
+        return self.val
+        
+    def set_val(self, val):
+        self.val = val
+    
+    def get_left(self):
+        return self.left
+    
+    def set_left(self, node):
+        self.left = node
+
+    def get_right(self):
+        return self.right
+    
+    def set_right(self, node):
+        self.right = node
+    
+    def add_left_num(self):
+        self.left_num = self.left_num+1
+    
+    def dec_left_num(self):
+        if self.left_num>0:
+            self.left_num = self.left_num-1
+    
+    def add_right_num(self):
+        self.right_num = self.right_num+1
+    
+    def dec_right_num(self):
+        if self.right_num>0:
+            self.right_num = self.right_num-1
+        
+
+class SearchTree:
+    root = None
+    
+    def insert(self, st_node):
+        if self.root = None:
+            root = st_node
+        else:
+            key = st_node.get_key()
+            temp_node = root
+            pre_temp_node = root
+            is_left = False
+            while temp_node!=None:
+                if key<temp_node.get_key():
+                    pre_temp_node = temp_node
+                    temp_node = temp_node.get_left()
+                    is_left = True
+                else:
+                    pre_temp_node = temp_node
+                    temp_node = temp_node.get_left()
+                    is_left = False
+            if is_left:
+                pre_temp_node.set_left(st_node)
+            else:
+                pre_temp_node.set_right(st_node)
+            
+    def remove(self, st_node):
+        
+        
+    def get(slef, key):
+        temp_node = root
+        while temp_node!=None:
+            if key == temp_node.get_key():
+                return temp_node.get_val()
+                
+            if key<temp_node.get_key():
+                temp_node = temp_node.get_left()
+            else:
+                temp_node = temp_node.get_right()
+        return None
