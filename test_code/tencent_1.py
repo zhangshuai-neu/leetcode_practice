0e4ca04e4778f6838d20fc11768064b1dd537ee5
zhangshuai <zhangshuaiisme@gmail.com>
2019-09-01 22:00:38 +0800

    some

--- test_code/tencent_1.py
+++ test_code/tencent_1.py
@@ -2,19 +2,21 @@ while True:
     try:
         case_num = int(input())
         for j in range(case_num):
-            print("debug j")
             num_map = {}
             #读取case并处理
             num_count = int(input())
             data = list(map(int,input().split()))
-            for d in range(data):
-                if d in num_map:
-                    num_map[d] = num_map[d]+1
+            for d in range(len(data)):
+                if data[d] in num_map:
+                    num_map[data[d]] = num_map[data[d]]+1
                 else:
-                    num_map.update({d:1})
-            
+                    num_map.update({data[d]:1})
+
+            print(num_map)
             num_list = num_map.values()
-            num_list.sort(reverse=False)
+            print(num_list)
+            num_list.sort()
+            print(num_list)
 
             result = "NO"
             left_sum = 0
@@ -24,6 +26,7 @@ while True:
                 right_sum = right_sum - num_list[i]
                 if left_sum == right_sum:
                     result = "YES"
+                    break
             print(result)
     except:
         break
